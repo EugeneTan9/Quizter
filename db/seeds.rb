0a5e41314bba88839644a9b8cd7acc88c1f4e220
David M <davidmosk@gmail.com>
2019-11-27 19:27:38 +0800

    start seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+    {email: "david@gmail.com", username: "david", password: "password", admin: false, dp: "https://web.aflia.net/wp-content/uploads/2018/12/dp_placeholder-275x300.jpg"},
+    {email: "eugene@gmail.com", username: "eugene", password: "password", admin: false},
+    {email: "kevin@gmail.com", username: "kevin", password: "password", admin: false},
+    {email: "admin@gmail.com", username: "testadmin", password: "password", admin: true}
+  ])
