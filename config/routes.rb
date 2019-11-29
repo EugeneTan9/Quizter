7a94ddfc238991ad00b6c27e79c219730c34ba41
David M <davidmosk@gmail.com>
2019-11-29 11:29:37 +0800

    add link to badges for creation from the edit page of the quiz

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :options
   resources :results
   resources :questions
-  resources :quizzes
-  resources :badges
+  resources :quizzes do
+    resources :badges
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
