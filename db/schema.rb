df477a1193a4349f21dc40b8201a4e2f58a01b92
David M <davidmosk@gmail.com>
2019-11-29 13:15:16 +0800

    quiz -> badges, quiz -> questions link, badges create, routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_063818) do
+ActiveRecord::Schema.define(version: 2019_11_29_050210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "badges", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.bigint "quiz_id", null: false
     t.string "title"
     t.integer "criteria"
@@ -24,7 +44,6 @@ ActiveRecord::Schema.define(version: 2019_11_28_063818) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["quiz_id"], name: "index_badges_on_quiz_id"
-    t.index ["user_id"], name: "index_badges_on_user_id"
   end
 
   create_table "badges_users", id: false, force: :cascade do |t|
@@ -89,8 +108,8 @@ ActiveRecord::Schema.define(version: 2019_11_28_063818) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "badges", "quizzes"
-  add_foreign_key "badges", "users"
   add_foreign_key "options", "questions"
   add_foreign_key "questions", "quizzes"
   add_foreign_key "quizzes", "users"
