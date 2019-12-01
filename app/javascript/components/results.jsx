f24169bc3d7f2e20a954b9973e766ca97d2a4774
EugeneTan9 <tyx.eugene@gmail.com>
2019-12-01 14:17:59 +0800

    able to end the quiz

---
+++ app/javascript/components/results.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Results extends React.Component{
+    constructor(){
+        super()
+        this.state={
+        }
+    }
+
+    render(){
+        return(
+            <div className={this.props.results_card}>
+            <h3>working fro results</h3>
+            </div>
+            )
+    }
+
+
+
+}
+
+
+
+export default Results;
