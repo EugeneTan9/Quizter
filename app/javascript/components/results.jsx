b7d4edce210670a1d2cacc2788c680414af65bfd
EugeneTan9 <tyx.eugene@gmail.com>
2019-12-02 09:41:34 +0800

    Able to view the results of the quiz

--- app/javascript/components/results.jsx
+++ app/javascript/components/results.jsx
@@ -10,7 +10,11 @@ class Results extends React.Component{
     render(){
         return(
             <div className={this.props.results_card}>
-            <h3>working fro results</h3>
+            <h2>Well Done completing the quiz!</h2>
+            <button onClick={()=>{this.props.getResults()}}>Time to see your results</button>
+            <h2>{this.props.score}</h2>
+            <img src={this.props.badge.img_url}/>
+            <h3>{this.props.noBadge}</h3>
             </div>
             )
     }
