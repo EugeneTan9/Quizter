99c79afc4b944b980846b1641812f7f60796c160
kevinngth <kevin.ng.th@gmail.com>
2019-12-02 15:21:17 +0800

    removed comments from questionform jsx

--- app/javascript/packs/QuestionForm.jsx
+++ app/javascript/packs/QuestionForm.jsx
@@ -34,115 +34,8 @@ class NewQuestion extends React.Component{
         this.setState({form: newForm})
         console.log(this.state.form)
     }
-//     getStarted(){
-//         this.state.hide_btn = "hide";
-//         console.log(this.state.hide_btn);
-//         this.setState({hide_btn:this.state.hide_btn});
-//         this.getQn();
-//         this.getQuiz();
-//     }
-
-//     getQuiz(){
-//         const url= '/quizzes/' + quiz_id + '/questions.json'
-
-//         axios.get(url)
-//             .then((response)=>{
-//                 const data = response.data
-//                 const length = data.filter(x=>x.quiz_id && x.body_type === "Q")
-//                 console.log(length.length);
-//                 this.state.quiz_length = length.length;
-//                 this.setState({quiz_length:this.state.quiz_length});
-//             }).catch((error)=> {
-//                 console.log(error);
-//             })
-//     }
-
-//     getQn(){
-//         const url= '/quizzes/' + quiz_id + '/questions.json'
-
-//         axios.get(url)
-//             .then((response) => {
-//                 qn ++;
-//                 const data = response.data
-//                 const filteredData = data.filter(x=>x.quiz_id == quiz_id && x.body_type === "Q" && x.question_num === qn)
-//                 const answer = data.filter(x=>x.quiz_id == quiz_id && x.body_type === "C" && x.question_num === qn)
-//                 this.state.answer = answer[0].body;
-//                 this.setState({answer:this.state.answer});
-//                 this.state.question = filteredData[0].body;
-//                 this.setState({question:this.state.question})
-//                 this.state.qn_num = qn;
-//                 this.setState({qn_num:this.state.qn_num});
-//                 this.getOption()
-//             }).catch((error)=> {
-//                 console.log(error);
-//             })
-//     }
-
-//     getOption(){
-//         const url= '/quizzes/' + quiz_id + '/questions.json'
-
-//         axios.get(url)
-//             .then((response)=>{
-//                 const data = response.data
-//                 const filteredOps = data.filter(x=>x.quiz_id == quiz_id && x.body_type !== "Q" && x.question_num === qn);
-//                 this.shuffle(filteredOps)
-//                 this.state.choices = filteredOps;
-//                 this.setState({choices:this.state.choices});
-//             }).catch((error)=>{
-//                 console.log(error);
-//             })
-//     }
-
-
-// // shuffling the options in an array so that it can be mapped in random sequence
-//     shuffle(array) {
-//       var currentIndex = array.length, temporaryValue, randomIndex;
-
-//       // While there remain elements to shuffle...
-//       while (0 !== currentIndex) {
-
-//         // Pick a remaining element...
-//         randomIndex = Math.floor(Math.random() * currentIndex);
-//         currentIndex -= 1;
-
-//         // And swap it with the current element.
-//         temporaryValue = array[currentIndex];
-//         array[currentIndex] = array[randomIndex];
-//         array[randomIndex] = temporaryValue;
-//       }
-
-//       return array;
-//     }
-
-//     saveAnw(){
-//         // console.log(this.state.quiz_length)
-//         if(event.target.innerHTML === this.state.answer){
-//             scoreArr.push(1);
-//             console.log(scoreArr);
-//         }else{
-//             scoreArr.push(0);
-//             console.log(scoreArr);
-//         }
-//         if(scoreArr.length === this.state.quiz_length){
-//             console.log("END");
-//             this.state.card = "hide";
-//             this.setState({card:this.state.card});
-//             this.state.results_card = "results_card";
-//             this.setState({results_card:this.state.results_card})
-//         }else{
-//         this.getQn()
-//         }
-//     }
 
     render(){
-       // console.log(this.state.choices);
-        // const choices = this.state.choices.map((choices, index)=>{
-        //     return(<div key={index}>
-        //         <button onClick={()=>{this.saveAnw(event)}}>{choices.body}</button>
-        //         </div>
-        //         )
-        // });
-
         return(<div className="d-flex justify-content-center">
             <div className="col-4 m-5">
                 <h1>Question #{this.state.question_num}</h1>
@@ -200,8 +93,8 @@ class NewQuestion extends React.Component{
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <NewQuestion/>,
-    document.body.appendChild(document.createElement('div')),
-  )
+    ReactDOM.render(
+        <NewQuestion/>,
+        document.body.appendChild(document.createElement('div')),
+    )
 })
