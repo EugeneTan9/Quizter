a5018452edc1579e52fb9b1d4230f02b76d469a4
kevinngth <kevin.ng.th@gmail.com>
2019-12-02 13:46:49 +0800

    completed cycle for user to add questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :verify_authenticity_token
   # GET /questions
   # GET /quizzes/:quiz_id/questions.json
   def index
--- app/javascript/packs/QuestionForm.jsx
+++ app/javascript/packs/QuestionForm.jsx
@@ -7,10 +7,33 @@ class NewQuestion extends React.Component{
     constructor(){
         super()
         this.state = {
-            form: []
+            form: [{},{},{},{},{}],
+            question_num: 1
         }
     }
 
+    async submitQuestions(){
+        try {
+            await this.state.form.forEach(item => {
+                const response = axios.post('/quizzes/'+window.location.pathname.split("/")[2]+'/questions', item);
+                console.log(response);
+            })
+            this.setState({
+                form: [{},{},{},{},{}],
+                question_num: this.state.question_num + 1
+            })
+        } catch (error) {
+                console.error(error);
+        }
+    }
+
+    changeState(e,i){
+        let newForm = this.state.form;
+        let body_type = ["Q","C","F","F","F"]
+        newForm[i] = {question_num: this.state.question_num, body: e.target.value, body_type: body_type[i]}
+        this.setState({form: newForm})
+        console.log(this.state.form)
+    }
 //     getStarted(){
 //         this.state.hide_btn = "hide";
 //         console.log(this.state.hide_btn);
@@ -122,17 +145,55 @@ class NewQuestion extends React.Component{
 
         return(<div className="d-flex justify-content-center">
             <div className="col-4 m-5">
-                <h1>New Question</h1>
+                <h1>Question #{this.state.question_num}</h1>
                 <h4>Question</h4>
-                <textarea class="form-control" placeholder="Question here" required></textarea>
-                <h4>Option 1</h4>
-                <input type="text" class="form-control" placeholder="Correct answer" required/>
-                <h4>Option 2</h4>
-                <input type="text" class="form-control" placeholder="False option" required/>
-                <h4>Option 3</h4>
-                <input type="text" class="form-control" placeholder="False option" required/>
-                <h4>Option 4</h4>
-                <input type="text" class="form-control" placeholder="False option" required/>
+                <textarea
+                    onChange={(e)=>{this.changeState(e,0)}}
+                    className="form-control"
+                    placeholder="Question here"
+                    value={this.state.form[0].body ? this.state.form[0].body : ""}
+                    required
+                />
+                <h4>Correct Answer</h4>
+                <input
+                    onChange={(e)=>{this.changeState(e,1)}}
+                    type="text"
+                    className="form-control"
+                    placeholder="Correct answer"
+                    value={this.state.form[1].body ? this.state.form[1].body : ""}
+                    required
+                />
+                <h4>False option</h4>
+                <input
+                    onChange={(e)=>{this.changeState(e,2)}}
+                    type="text"
+                    className="form-control"
+                    placeholder="False option"
+                    value={this.state.form[2].body ? this.state.form[2].body : ""}
+                    required
+                />
+                <h4>False option</h4>
+                <input
+                    onChange={(e)=>{this.changeState(e,3)}}
+                    type="text"
+                    className="form-control"
+                    placeholder="False option"
+                    value={this.state.form[3].body ? this.state.form[3].body : ""}
+                    required
+                />
+                <h4>False option</h4>
+                <input
+                    onChange={(e)=>{this.changeState(e,4)}}
+                    type="text"
+                    className="form-control"
+                    placeholder="False option"
+                    value={this.state.form[4].body ? this.state.form[4].body : ""}
+                    required
+                />
+                <div className="d-flex justify-content-around my-3">
+                    <button onClick={()=>{this.submitQuestions()}} className="btn btn-primary btn-sm">Add Question</button>
+                    <a href={'/quizzes/'+window.location.pathname.split("/")[2]}><button className="btn btn-primary btn-sm">Review Quiz</button></a>
+                </div>
             </div>
         </div>)
     }
